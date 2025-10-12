c8d5014dd812e420c9f9c68b3a930b49597aac0b
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-12 20:45:02 +1100

    Refactor to helper function DRY

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,15 +1,17 @@
 // Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
 // Round to 1 decimal place
+const roundToDecimal = (num) => Math.round(num * 10) / 10;
+
 const convertToCelsius = function(fahrenheit) {
   // x °F ≘ (x − 32) × ⁠5 / 9°C
 
-  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10 ;
+  return roundToDecimal((fahrenheit - 32) * 5/9);
 };
 
 const convertToFahrenheit = function(celsius) {
   // x °C ≘ (x × ⁠9 / 5 + 32) °F
 
-  return Math.round(((celsius * 9/5) + 32) * 10 ) / 10;
+  return roundToDecimal((celsius * 9/5) + 32);
 };
 
 // Do not edit below this line
