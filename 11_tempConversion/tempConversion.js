d9eb6cdfbcf2483ddb48c1e9a347231667aead06
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-12 20:41:02 +1100

    Converting to and from fahrenheit and celsius rounding

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
+// Round to 1 decimal place
+const convertToCelsius = function(fahrenheit) {
+  // x °F ≘ (x − 32) × ⁠5 / 9°C
+
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10 ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // x °C ≘ (x × ⁠9 / 5 + 32) °F
+
+  return Math.round(((celsius * 9/5) + 32) * 10 ) / 10;
 };
 
 // Do not edit below this line
