7ee0a4f74c5deca8a1450641f843d3a776184f0a
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-12 19:31:55 +1100

    using .filter array method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,19 +1,21 @@
 // Implement a function that takes an array and some other arguments then removes the other arguments from that array:
 // See if you can make use of some built-in array methods in this exercise
 const removeFromArray = function(arr, ...values) {
-    for (let value of values)
-    {
-        let valueIndex = arr.findIndex(element => element === value);
+    // for (let value of values)
+    // {
+    //     let valueIndex = arr.findIndex(element => element === value);
 
-        while (valueIndex !== -1)
-        {
-            arr.splice(valueIndex, 1);
+    //     while (valueIndex !== -1)
+    //     {
+    //         arr.splice(valueIndex, 1);
             
-            valueIndex = arr.findIndex(element => element === value);
-        }
-    }
+    //         valueIndex = arr.findIndex(element => element === value);
+    //     }
+    // }
 
-    return arr;
+    // return arr;
+
+    return arr.filter(element => !values.includes(element));
 };
 
 // Do not edit below this line
