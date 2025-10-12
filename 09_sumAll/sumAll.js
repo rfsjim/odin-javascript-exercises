cddfd0b7bdaab1edbd5cc8f4125f6730745839a9
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-12 20:04:36 +1100

    Adds Sum all positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them
+const sumAll = function(a, b) {
+    if (Number.isNaN(a) || a <= 0 || !Number.isInteger(a) ||
+        b <= 0 || Number.isNaN(b) || !Number.isInteger(b))
+    {
+        return 'ERROR';
+    }
 
+    if (b < a)
+    {
+        let swap = a;
+        a = b;
+        b = swap;
+    }
+
+    let sum = 0;
+
+    for (let i = a; i <= b; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
