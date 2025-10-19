d0b807170d1834c99dcb4fbca8f6de311e09e8a8
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-19 12:21:22 +1100

    feat(book objects details): Function get details from book object

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+// given an array of objects that represent books with an author and a title that looks like this
+const getTheTitles = function(bookObjectArray) {
+    return bookObjectArray.map(book => book.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
 // Do not edit below this line
 module.exports = getTheTitles;
