aed4e6ab52148e49e1ec7213f7bb46154bca5d44
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-19 12:14:31 +1100

    feat(fibonacci) Adds Function to return given element of fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+/**
+ * A function that returns a specific member of the Fibonacci sequence
+ * @param {number} number requested specific member number from the Fibonacci sequence
+ * @returns {number} fibonacci number
+ */
+const fibonacci = function(number) {
+    if (typeof number !== 'number')
+    {
+        number = parseInt(number);
+    }
 
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+
+    const fibonacciNumber = [0, 1];
+
+    for (let i = 2; i <= number; i++)
+    {
+        fibonacciNumber[i] = fibonacciNumber[i - 1] + fibonacciNumber[i - 2];
+    }
+
+    return fibonacciNumber[number];
 };
 
 // Do not edit below this line
