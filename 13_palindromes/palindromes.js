2551703ae865d6b88f782af27740269132352697
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-19 11:44:42 +1100

    feat(palindromes): Adds function to test for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+/**
+ * Tests if given string is a palindrome
+ * @param {string} string
+ * @returns {boolean} true if palindrome, false if not 
+ */
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/g, "").split("");
 
+    let reverseString = [];
+
+    for (let i = string.length - 1; i >= 0; i--)
+    {
+        reverseString.push(string[i]);
+    }
+
+    string = string.join();
+    reverseString = reverseString.join();
+
+    if (string === reverseString) return true;
+    return false;
 };
 
 // Do not edit below this line
