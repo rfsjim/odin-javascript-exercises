1d5b127422f1622dda1293dc317784abdb3b1dbe
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-22 20:42:09 +1100

    Adds Reduce() Returning Oldest Person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+// Given an array of objects representing people with a birth and death year, return the oldest person
 
-};
+const findTheOldest = function(array) { 
+    let oldestIndex, oldestSoFar = 0;
+    return array.reduce((oldest, person, index) => { 
+        let deathYear;
+        if (person.yearOfDeath === null)
+        { 
+            deathYear = new Date().getFullYear(); 
+        } else 
+            { 
+                deathYear = person.yearOfDeath; 
+            }
+            let age = deathYear - person.yearOfBirth;
+            if ((age) > oldestSoFar)
+                { 
+                    oldestSoFar = (age);
+                    oldestIndex = index;
+                }
+                return array[oldestIndex];
+        },
+        {})
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
