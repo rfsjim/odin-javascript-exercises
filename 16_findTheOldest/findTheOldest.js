395479dae2a38eaee127fad56f035a170602eaf2
rfsjim <5404425+rfsjim@users.noreply.github.com>
2025-10-22 20:57:34 +1100

    feat: Adds Optimal refinement with helper function for calculating age

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,16 +1,14 @@
 // Given an array of objects representing people with a birth and death year, return the oldest person
+const getAge = (person, year = new Date().getFullYear()) => 
+  (person.yearOfDeath ?? year) - person.yearOfBirth;
+
 const findTheOldest = function(array) {
-    return array.reduce((oldest, person) => {
-        const currentYear = new Date().getFullYear();
-
-        const personDeath = person.yearOfDeath ?? currentYear;
-        const oldestDeath = oldest.yearOfDeath ?? currentYear;
-
-        const personAge = personDeath - person.yearOfBirth;
-        const oldestAge = oldestDeath - oldest.yearOfBirth;
-
-        return personAge > oldestAge ? person : oldest;
-    });
+  const currentYear = new Date().getFullYear();
+  return array.reduce((oldest, person) => {
+    return getAge(person, currentYear) > getAge(oldest, currentYear)
+      ? person
+      : oldest;
+  });
 };
 
 // Do not edit below this line
